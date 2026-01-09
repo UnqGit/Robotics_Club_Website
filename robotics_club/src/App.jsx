ac47ee5d49bdf5012a0d14ccbcbc849e92c9bdbf
Kartikey Gangwar <kartikeygangwar90@gmail.com>
2026-01-09 18:01:53 +0530

    Beginning of the project

---
+++ robotics_club/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './appStyle.css'
+
+const App = () => {
+  return (
+    <div className='main--container--mn'>
+      {/* The Navbar section */}
+      <div className="navbar--nb">
+
+        <div className="logo--point">
+
+        </div>
+        <div className="navigating--navbar">
+
+        </div>
+      </div>
+      <h1>Robotics club</h1>
+    </div>
+  )
+}
+
+export default App
